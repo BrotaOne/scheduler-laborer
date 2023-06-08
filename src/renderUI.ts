99dea552a360346309ecbe0840f0735e90c1ce59
gongping01 <gongping01@baidu.com>
2023-06-08 10:53:21 +0800

    feature: 增加lint-staged

--- src/renderUI.ts
+++ src/renderUI.ts
@@ -27,7 +27,7 @@ interface State {
 // Max 31 bit integer. The max integer size in V8 for 32-bit systems.
 // Math.pow(2, 30) - 1
 // 0b111111111111111111111111111111
-var maxSigned31BitInt = 1073741823;
+const maxSigned31BitInt = 1073741823;
 
 const defaultState = { name: '摸鱼', duration: maxSigned31BitInt, priority: LowPriority };
 
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,9 +5,10 @@ declare module 'scheduler' {
     export const unstable_LowPriority: 4;
     export const unstable_NormalPriority: 3;
     export const unstable_UserBlockingPriority: 2;
+    type Callback = (didTimeout: boolean) => Callback | void;
     export type Task = {
         id: number;
-        callback: any;
+        callback: Callback;
         priorityLevel: PriorityLevel;
         // priorityLevel: 0 | 1 | 2 | 3 | 4 | 5;
         startTime: number;
@@ -16,9 +17,15 @@ declare module 'scheduler' {
     };
 
     export const unstable_getFirstCallbackNode: () => Task;
-    export const unstable_scheduleCallback: (priorityLevel: PriorityLevel, callback: any, options?: any)=> Task;
+    export const unstable_scheduleCallback: (
+        priorityLevel: PriorityLevel,
+        callback: Callback,
+        options?: {
+            delay: number;
+        }
+    ) => Task;
     export const unstable_shouldYield: () => boolean;
-    export const unstable_cancelCallback: (task: any) => void;
+    export const unstable_cancelCallback: (task: Task) => void;
 
     export type CallbackNode = Task;
 }
